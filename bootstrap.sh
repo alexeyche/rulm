12b9de43cc90c9a66c1f4a70b85e154322fbf5a7
Alexey Chernyshev <aleksei@spotify.com>
2023-05-27 22:07:43 +0200

    Add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ex
+
+apt update
+
+apt install -y vim git-lfs screen
+
+pushd /workspace/rulm
+pip install -r requirements.txt &
+popd
+
+pushd /workspace
+wget --quiet --show-progress https://github.com/Run-Pod/runpodctl/releases/download/v1.9.0/runpodctl-linux-amd -O runpodctl && chmod +x runpodctl && mv runpodctl /usr/bin/runpodctl
+popd
+
+mkdir /workspace/rulm/self_instruct/models
+
+pushd /workspace/rulm/self_instruct/models
+git clone https://huggingface.co/AlexeyChe/llama-7b-lora
+popd
+
+
+wait
